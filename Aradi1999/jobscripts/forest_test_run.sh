d5f877f70e53ed3cf102c5d4b9aa2e71435f5108
Ivan Raikov <ivan.g.raikov@gmail.com>
2014-12-23 13:27:03 -0800

    added initial cluster job script

---
+++ Aradi1999/jobscripts/forest_test_run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#$ -q som,asom,pub64,free64
+#$ -pe openmp 64
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#$ -N DGC_forest_test
+#$ -o ./jobscripts/forest_test_run.$JOB_ID.o
+#$ -R y
+
+module load neuron/7.3
+mpiexec -np 64 nrniv -mpi -nobanner -nogui ./DGC_test_from_forest_na8st.hoc
+#cp ./jobscripts/testrun02* ./results/testrun02/
+
