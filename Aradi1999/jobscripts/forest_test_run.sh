8c24a8fa28fe700b73cef0bba6da0bb803870154
Ivan Raikov <iraikov@hpc-login-1-3.local>
2015-01-07 16:01:26 -0800

    small tweaks to cluster job script

--- Aradi1999/jobscripts/forest_test_run.sh
+++ Aradi1999/jobscripts/forest_test_run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-#$ -q free64
+#$ -q free64,pub64
 #$ -t 1-20
 #$ -cwd
 #$ -j y
 #$ -S /bin/bash
 #$ -N DGC_forest_test_Aradi1999_na8st
 #$ -o ./results/forest_test_Aradi1999_na8st.$JOB_ID.o
-#$ -R y
+
 
 module load neuron/7.3
 nrniv -nobanner -nogui -c "batch_size=20" -c "task_id=$SGE_TASK_ID - 1" ./DGC_test_from_forest_na8st.hoc
