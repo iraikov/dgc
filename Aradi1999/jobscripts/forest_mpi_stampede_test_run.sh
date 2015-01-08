c5dfca6e00c842134135acf45c5531b8ae6d188d
Ivan Raikov <iraikov@login3.stampede.tacc.utexas.edu>
2015-01-08 16:12:30 -0600

    added stampede test script

---
+++ Aradi1999/jobscripts/forest_mpi_stampede_test_run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH -J DGC_forest_test # name of job
+#SBATCH -o DGC_forest_test.%j.o # output file (errors can be placed in this file, too). %j expands to the job id number.
+#SBATCH -n 20 # number of cores
+#SBATCH -p development # queue to use
+#SBATCH -t 02:00:00 # time limit
+#SBATCH --mail-user=ivan.g.raikov@gmail.com # where to send notifications
+#SBATCH --mail-type=ALL # types of mail notifications to receive
+
+ibrun tacc_affinity  x86_64/special -mpi ./DGC_forest_test_Aradi1999_na8st.hoc
+
