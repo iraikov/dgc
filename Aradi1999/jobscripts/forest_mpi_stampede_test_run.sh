b1a17f94fe35a62c727aba229dcea581713c1443
Ivan Raikov <iraikov@login4.stampede.tacc.utexas.edu>
2015-01-09 17:32:34 -0600

    bug fixes in stampede config

--- Aradi1999/jobscripts/forest_mpi_stampede_test_run.sh
+++ Aradi1999/jobscripts/forest_mpi_stampede_test_run.sh
@@ -2,11 +2,15 @@
 
 #SBATCH -J DGC_forest_test # name of job
 #SBATCH -o DGC_forest_test.%j.o # output file (errors can be placed in this file, too). %j expands to the job id number.
-#SBATCH -n 1 # number of cores
-#SBATCH -p development # queue to use
-#SBATCH -t 00:10:00 # time limit
+#SBATCH -n 20 # number of cores
+#SBATCH -p normal # queue to use
+#SBATCH -t 02:00:00 # time limit
 #SBATCH --mail-user=ivan.g.raikov@gmail.com # where to send notifications
 #SBATCH --mail-type=ALL # types of mail notifications to receive
 
-ibrun tacc_affinity ./x86_64/special -mpi -c "strdef forest_config" -c "forest_config=\"DGC_stampede_forest_config.hoc\"" \
- ./DGC_mpi_test_from_forest_na8st.hoc
+ibrun tacc_affinity ./x86_64/special -mpi - <<EOF
+strdef forest_config
+forest_config="DGC_stampede_forest_config.hoc"
+load_file("DGC_mpi_test_from_forest_na8st.hoc")
+EOF
+
