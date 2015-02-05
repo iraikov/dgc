b0a31921eb7fa4651c5bc6e326f74200cba93cf3
Ivan Raikov <ivan.g.raikov@gmail.com>
2015-02-05 10:24:24 -0800

    separate configuration for passive forest test

---
+++ Mateos-Aparicio2014/jobscripts/forest_test_run_passive.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#$ -q free64,pub64
+#$ -t 1-20
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#$ -N DGC_forest_test_MateosAparicio2014_passive_na8st
+#$ -o ./results/forest_test_MateosAparicio2014_passive_na8st.$JOB_ID.o
+
+
+module load neuron/7.3
+mkdir -p ./results
+nrniv -nobanner -nogui -c "batch_size=20" -c "task_id=$SGE_TASK_ID - 1" \
+-c "strdef forest_config" -c 'forest_config="DGC_hpc_forest_config.hoc"' \
+./DGC_serial_test_from_forest_passive_na8st.hoc
