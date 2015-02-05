85360034bc03cf6f3ada7cf5aa8b0d40feb7b040
Ivan Raikov <ivan.g.raikov@gmail.com>
2015-02-05 11:53:43 -0800

    added surface area field

--- analysis/maketbl.sh
+++ analysis/maketbl.sh
@@ -13,7 +13,7 @@ ncells=1000
 
 path_prefix="$2"
 
-echo "## gid,DC_input_resistance,vmin,vtau0,tau0,maximum_Vsoma,minimum_Vsoma,threshold,AP_amplitude_rel_threshold,AHP,Rel_AP_amplitude_dendrite_0,Rel_AP_amplitude_dendrite_1,Rel_AP_amplitude_dendrite_2,number_of_spikes,mean_FR,mean_ISI,stdev_ISI,adaptation_ISI1,adaptation_ISI2,adaptation_ISI3" > $tblpath
+echo "## gid,DC_input_resistance,dendarea,vmin,vtau0,tau0,maximum_Vsoma,minimum_Vsoma,threshold,AP_amplitude_rel_threshold,AHP,Rel_AP_amplitude_dendrite_0,Rel_AP_amplitude_dendrite_1,Rel_AP_amplitude_dendrite_2,number_of_spikes,mean_FR,mean_ISI,stdev_ISI,adaptation_ISI1,adaptation_ISI2,adaptation_ISI3" > $tblpath
 
 for (( i = 0; i < ncells; i++ )) do
 
@@ -28,6 +28,7 @@ for (( i = 0; i < ncells; i++ )) do
     vmin=`grep "vmin" $passive_results_path | cut -f2 -d' '`
     vtau0=`grep "vtau0" $passive_results_path | cut -f2 -d' '`
     tau0=`grep "^tau0" $passive_results_path | cut -f2 -d' '`
+    tau0=`grep "dendritic surface area" $passive_results_path | cut -f5 -d' '`
 
     maximum_Vsoma=`grep "maximum Vsoma" $single_ap_results_path | cut -f3 -d' '`
     minimum_Vsoma=`grep "minimum Vsoma" $single_ap_results_path | cut -f3 -d' '`
