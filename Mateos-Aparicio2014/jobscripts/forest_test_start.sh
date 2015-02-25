cfa39d08d828582122183c1c2eaee55d21087d35
Ivan Raikov <ivan.g.raikov@gmail.com>
2015-02-25 13:46:09 -0800

    added a start script to create the config file and submit the job

---
+++ Mateos-Aparicio2014/jobscripts/forest_test_start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ncells=1000
+data_path=/data/users/iraikov/model/DGC_forest/dat
+results_path="/pub/iraikov/DGC_forest_test_results/`date +'%Y%m%d%H%M'`"
+
+echo "$ncells" > DGC_forest_hpc.config
+echo "$data_path" >>  DGC_forest_hpc.config
+echo "$results_path" >>  DGC_forest_hpc.config
