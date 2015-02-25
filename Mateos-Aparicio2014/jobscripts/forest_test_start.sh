e007f9e8ef06df37f45f5531b396cf504ac576b5
Ivan Raikov <ivan.g.raikov@gmail.com>
2015-02-25 13:52:59 -0800

    added start script

--- Mateos-Aparicio2014/jobscripts/forest_test_start.sh
+++ Mateos-Aparicio2014/jobscripts/forest_test_start.sh
@@ -7,3 +7,5 @@ results_path="/pub/iraikov/DGC_forest_test_results/`date +'%Y%m%d%H%M'`"
 echo "$ncells" > DGC_forest_hpc.config
 echo "$data_path" >>  DGC_forest_hpc.config
 echo "$results_path" >>  DGC_forest_hpc.config
+
+qsub ./jobscripts/forest_test_run.sh
