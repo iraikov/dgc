a2896ad05dd5d06a3a6c836d5842b1ff54b4316d
Ivan Raikov <ivan.g.raikov@gmail.com>
2015-02-25 12:11:56 -0800

    transitioning to generated test config file

--- Mateos-Aparicio2014/jobscripts/forest_test_run.sh
+++ Mateos-Aparicio2014/jobscripts/forest_test_run.sh
@@ -8,9 +8,13 @@
 #$ -N DGC_forest_test_MateosAparicio2014_na8st
 #$ -o ./results/forest_test_MateosAparicio2014_na8st.$JOB_ID.o
 
-
 module load neuron/7.3
-mkdir -p ./results
+results_path="/pub/iraikov/DGC_forest_test_results/`date +'%Y%m%d%H%M'`"
+export results_path
+echo nrniv -nobanner -nogui -c "batch_size=20" -c "task_id=$SGE_TASK_ID - 1" \
+-c "strdef forest_config" -c 'forest_config="DGC_forest_hpc.config"' \
+./DGC_serial_test_from_forest_na8st.hoc
+
 nrniv -nobanner -nogui -c "batch_size=20" -c "task_id=$SGE_TASK_ID - 1" \
--c "strdef forest_config" -c 'forest_config="DGC_hpc_forest_config.hoc"' \
+-c "strdef forest_config" -c 'forest_config="DGC_forest_hpc.config"' \
 ./DGC_serial_test_from_forest_na8st.hoc
