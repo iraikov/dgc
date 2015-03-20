2e0dba2830b04c627c036202a057480f501c5934
Ivan Raikov <ivan.g.raikov@gmail.com>
2015-03-20 11:00:50 -0700

    modifications to main forest test script

--- Mateos-Aparicio2014/jobscripts/forest_serial_test_start.sh
+++ Mateos-Aparicio2014/jobscripts/forest_serial_test_start.sh
@@ -2,12 +2,12 @@
 
 config_path=./config/DGC_forest_hpc.config
 
-ncells=22321
-data_path=/pub/iraikov/dentate/slice0000/GC
-results_path="/pub/iraikov/DGC_forest_test_results/`date +'%Y%m%d%H%M'`"
+ncells=1000
+data_path=/pub/iraikov/dentate/test0000/GC
+results_path="/pub/iraikov/DGC_forest_synapse_test_results/`date +'%Y%m%d%H%M'`"
 
 echo "$ncells" > $config_path
 echo "$data_path" >> $config_path
 echo "$results_path" >> $config_path
 
-qsub ./jobscripts/forest_serial_test_run.sh
+qsub ./jobscripts/forest_serial_synapse_test_run.sh
