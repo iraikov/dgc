eed666df62fdf2971ac010e989719c2657f187f2
Ivan Raikov <ivan.g.raikov@gmail.com>
2015-04-06 15:39:19 -0700

    corrected serial test start to call the main test script

--- Mateos-Aparicio2014/jobscripts/forest_serial_test_start.sh
+++ Mateos-Aparicio2014/jobscripts/forest_serial_test_start.sh
@@ -3,11 +3,11 @@
 config_path=./config/DGC_forest_hpc.config
 
 ncells=1000
-data_path=/pub/iraikov/dentate/test0000/GC
+data_path=/pub/iraikov/DGC_forest/110/GC
 results_path="/pub/iraikov/DGC_forest_synapse_test_results/`date +'%Y%m%d%H%M'`"
 
 echo "$ncells" > $config_path
 echo "$data_path" >> $config_path
 echo "$results_path" >> $config_path
 
-qsub ./jobscripts/forest_serial_synapse_test_run.sh
+qsub ./jobscripts/forest_serial_test_run.sh
