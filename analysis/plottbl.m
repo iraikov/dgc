cc93fa7f2bc483ed175496e3bcb73549e8cfda62
Ivan Raikov <ivan.g.raikov@gmail.com>
2015-04-13 16:22:29 -0700

    added synapse parameters to plotting script

--- analysis/plottbl.m
+++ analysis/plottbl.m
@@ -46,7 +46,20 @@ hist(spike_ahp,50);
 title(sprintf("Fast AHP [mV]; mean = %g std = %g",
       mean(spike_ahp),std(spike_ahp)));
 
-s = subplot(3, 2, 6)
+print (h, "DGC_results1.pdf", "-dpdf")
+
+h = figure(2);
+
+subplot(2, 2, 1)
+hist(isi_adaptation4,50);
+title(sprintf("ISI adaptation 4; mean = %g std = %g",
+      mean(isi_adaptation4),std(isi_adaptation4)));
+
+subplot(2, 2, 2)
+hist(number_of_spikes,20);
+title(sprintf("Number of spikes; mean = %g",mean(number_of_spikes)));
+
+s = subplot(2, 2, 3)
 rel_amplitude_dend1_mean = mean(rel_amplitude_dend1);
 rel_amplitude_dend1_stdev = std(rel_amplitude_dend1);
 rel_amplitude_dend2_mean = mean(rel_amplitude_dend2);
@@ -76,31 +89,81 @@ set(s,'XTick',0:50:250);
 set(s,'YTick',0:0.2:1);
 title("Amplitude of dendritic AP rel. to soma");
 
-print (h, "DGC_results1.pdf", "-dpdf")
-
-h = figure(2);
-
-subplot(2, 2, 1)
-hist(isi_adaptation4,50);
-title(sprintf("ISI adaptation 4; mean = %g std = %g",
-      mean(isi_adaptation4),std(isi_adaptation4)));
-
-subplot(2, 2, 2)
-hist(isi_adaptation3,50);
-title(sprintf("ISI adaptation 3; mean = %g std = %g",
-      mean(isi_adaptation3),std(isi_adaptation3)));
-
-subplot(2, 2, 3)
-hist(number_of_spikes,20);
-title(sprintf("Number of spikes; mean = %g",mean(number_of_spikes)));
-
-subplot(2, 2, 4)
-hist(mean_firing_rate,50);
-title(sprintf("Mean firing rate [Hz]"));
-
-%subplot(2, 2, 4)
-%hist(mean_isi,50);
-%title(sprintf("Mean ISI [ms]"));
-
-
 print (h, "DGC_results2.pdf", "-dpdf")
+
+% subplot(2, 3, 1)
+% hist(mean_firing_rate,50);
+% title(sprintf("Mean firing rate [Hz]"));
+
+% subplot(2, 2, 4)
+% hist(mean_isi,50);
+% title(sprintf("Mean ISI [ms]"));
+
+h = figure(3);
+
+HC_amp=DGC_results(:,25);
+HC_rise=DGC_results(:,26);
+HC_decay=DGC_results(:,27);
+BC_amp=DGC_results(:,28);
+BC_rise=DGC_results(:,29);
+BC_decay=DGC_results(:,30);
+HCC_amp=DGC_results(:,31);
+HCC_rise=DGC_results(:,32);
+HCC_decay=DGC_results(:,33);
+AA_amp=DGC_results(:,34);
+AA_rise=DGC_results(:,35);
+AA_decay=DGC_results(:,36);
+NGFC_GABAA_amp=DGC_results(:,36);
+NGFC_GABAA_rise=DGC_results(:,37);
+NGFC_GABAA_decay=DGC_results(:,38);
+
+subplot(3, 3, 1)
+hist(HC_amp,50);
+title(sprintf("HIPP syn amp; \n mean = %g std = %g",
+      mean(HC_amp),std(HC_amp)));
+
+subplot(3, 3, 2)
+hist(HC_rise,50);
+title(sprintf("HIPP syn rise; \n mean = %g std = %g",
+      mean(HC_rise),std(HC_rise)));
+
+subplot(3, 3, 3)
+hist(HC_decay,50);
+title(sprintf("HIPP syn decay; \n mean = %g std = %g",
+      mean(HC_decay),std(HC_decay)));
+
+
+subplot(3, 3, 4)
+hist(BC_amp,50);
+title(sprintf("PVBC syn amp; \n mean = %g std = %g",
+      mean(BC_amp),std(BC_amp)));
+
+subplot(3, 3, 5)
+hist(BC_rise,50);
+title(sprintf("PVBC syn rise; \n mean = %g std = %g",
+      mean(BC_rise),std(BC_rise)));
+
+subplot(3, 3, 6)
+hist(BC_decay,50);
+title(sprintf("PVBC syn decay; \n mean = %g std = %g",
+      mean(BC_decay),std(BC_decay)));
+
+
+subplot(3, 3, 7)
+hist(AA_amp,50);
+title(sprintf("AA syn amp; \n mean = %g std = %g",
+      mean(AA_amp),std(AA_amp)));
+
+subplot(3, 3, 8)
+hist(AA_rise,50);
+title(sprintf("AA syn rise; \n mean = %g std = %g",
+      mean(AA_rise),std(AA_rise)));
+
+subplot(3, 3, 9)
+hist(AA_decay,50);
+title(sprintf("AA syn decay; \n mean = %g std = %g",
+      mean(AA_decay),std(AA_decay)));
+
+
+
+print (h, "DGC_results3.pdf", "-dpdf")
