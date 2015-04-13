65d6c3ed02007cad422a85c5e232cbc731533921
Ivan Raikov <ivan.g.raikov@gmail.com>
2015-04-13 13:28:03 -0700

    local updates on hpc

--- Mateos-Aparicio2014/jobscripts/forest_serial_test_passive_start.sh
+++ Mateos-Aparicio2014/jobscripts/forest_serial_test_passive_start.sh
@@ -2,9 +2,11 @@
 
 config_path=./config/DGC_forest_hpc_passive.config
 
+forest=110
+
 ncells=1000
-data_path=/pub/iraikov/DGC_forest/110/GC
-results_path="/pub/iraikov/DGC_forest_test_results/passive/`date +'%Y%m%d%H%M'`"
+data_path=/pub/iraikov/DGC_forest/$forest/GC
+results_path="/pub/iraikov/DGC_forest_test_results/passive/$forest/`date +'%Y%m%d%H%M'`"
 
 echo "$ncells" > $config_path
 echo "$data_path" >> $config_path
