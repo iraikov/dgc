90fb10928c869f55aa16650f71387df3b7b53763
Ivan Raikov <ivan.g.raikov@gmail.com>
2015-04-13 16:21:53 -0700

    updated parameters and tests

--- Mateos-Aparicio2014/jobscripts/forest_serial_test_passive_run.sh
+++ Mateos-Aparicio2014/jobscripts/forest_serial_test_passive_run.sh
@@ -13,10 +13,6 @@ module load neuron/7.4alpha
 
 set -x
 
-results_path="/pub/iraikov/DGC_forest_test_results/`date +'%Y%m%d%H%M'`"
-export results_path
-mkdir -p $results_path
-
 nrniv -nobanner -nogui -c "batch_size=100" -c "task_id=$SGE_TASK_ID - 1" \
 -c "strdef forest_config" -c 'forest_config="./config/DGC_forest_hpc_passive.config"' \
 ./DGC_serial_test_from_forest_passive_na8st.hoc
