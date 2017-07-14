421af7b6dd714348690ba59546d5c8b2e52f5ee2
Ivan Raikov <ivan.g.raikov@gmail.com>
2017-07-14 12:19:28 -0700

    reworking to allow for merging all test results before writing to file

--- Mateos-Aparicio2014/DGC_test_passive_na8st_run.py
+++ Mateos-Aparicio2014/DGC_test_passive_na8st_run.py
@@ -8,10 +8,6 @@ from mpi4py import MPI # Must come before importing NEURON
 from neuron import h
 from neuroh5.io import read_tree_selection
 
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
-
 def new_cell (template_name, local_id=0, gid=0, dataset_path="", neurotree_dict={}):
     h('objref cell, vx, vy, vz, vradius, vlayer, vsection, secnodes, vsrc, vdst')
     h.vx       = neurotree_dict['x']
@@ -27,6 +23,25 @@ def new_cell (template_name, local_id=0, gid=0, dataset_path="", neurotree_dict=
     h(hstmt)
     return h.cell
 
+def hoc_results_to_python(hoc_results):
+    results_dict = {}
+    for i in xrange(0, int(hoc_results.count())):
+        vect   = hoc_results.o(i)
+        gid    = int(vect.x[0])
+        pyvect = vect.to_python()
+        results_dict[gid] = pyvect[1:]
+    hoc_results.remove_all()
+    return results_dict
+
+def write_results(results, filepath, header):
+    f = open(filepath,'w')
+    f.write(header+'\n')
+    for item in results:
+        for (gid, vect) in item.iteritems():
+            f.write (str(gid)+"\t")
+            f.write (("\t".join(['{:0.3f}'.format(i) for i in vect])) + "\n")
+    f.close()
+    
 @click.command()
 @click.option("--template-path", required=True, type=click.Path(exists=True, file_okay=False, dir_okay=True))
 @click.option("--forest-path", required=True, type=click.Path(exists=True, file_okay=True, dir_okay=False))
@@ -34,6 +49,10 @@ def new_cell (template_name, local_id=0, gid=0, dataset_path="", neurotree_dict=
 @click.option('--selection', default=[], callback=lambda _,__,x: map(int, x.split(',')) if x else [])
 @click.option("--selection-file", default=None, type=click.Path(exists=True, file_okay=True, dir_okay=False))
 def main(template_path, forest_path, results_path, selection, selection_file):
+
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
 
     h.load_file(template_path+"/"+"DGC_Tests_from_file_passive_na8st.hoc")
 
@@ -49,17 +68,53 @@ def main(template_path, forest_path, results_path, selection, selection_file):
         f.close()
 
     myselection = [selection[i] for i in xrange(rank,len(selection),size)]
+
+    print 'rank %d: myselection = ' % rank, myselection
     
     pop_name = "GC"
     (trees, _) = read_tree_selection (MPI._addressof(comm), forest_path, pop_name, myselection)
+
+    h('objref results_passive, results_single_ap, results_threshold, results_ap_rate')
+    h.results_passive   = h.List()
+    h.results_single_ap = h.List()
+    h.results_threshold = h.List()
+    h.results_ap_rate   = h.List()
     
     for (gid, tree) in trees.iteritems():
         cell = new_cell ("DGC", neurotree_dict=tree)
-        h.passive_test(h.cell, results_path, gid)
-        h.single_ap_test(h.cell, results_path, gid)
-        h.threshold_test(h.cell, results_path, gid)
-        h.ap_rate_test(h.cell, results_path, gid, 0.3)
-    
+        h.passive_test(h.cell, h.results_passive, gid)
+        h.single_ap_test(h.cell, h.results_single_ap, gid)
+        h.threshold_test(h.cell, h.results_threshold, gid)
+        h.ap_rate_test(h.cell, h.results_ap_rate, gid, 0.3)
+
+    results_dict_passive   = hoc_results_to_python(h.results_passive)
+    results_dict_single_ap = hoc_results_to_python(h.results_single_ap)
+    results_dict_threshold = hoc_results_to_python(h.results_threshold)
+    results_dict_ap_rate   = hoc_results_to_python(h.results_ap_rate)
+
+    all_results_passive = comm.gather(sendobj=results_dict_passive, root=0)
+    if rank==0:
+        write_results(all_results_passive,
+                    results_path+"/"+"DGC_tests_results_passive.dat",
+                    "# Gid, input resistance, vmin,  vtau0, tau0, dendritic surface area")
+
+    all_results_single_ap = comm.gather(sendobj=results_dict_single_ap, root=0)
+    if rank==0:
+        write_results(all_results_single_ap,
+                    results_path+"/"+"DGC_tests_results_single_ap.dat",
+                    "# Gid, vmax, vmin, vdend0, vdend1, vdend2, vdend3, vdend4")
+
+    all_results_threshold = comm.gather(sendobj=results_dict_threshold, root=0)
+    if rank==0:
+        write_results(all_results_threshold,
+                    results_path+"/"+"DGC_tests_results_threshold.dat",
+                    "# Gid, vmax, vmin, v threshold, t threshold, amplitude, ahp")
+
+    all_results_ap_rate = comm.gather(sendobj=results_dict_ap_rate, root=0)
+    if rank==0:
+        write_results(all_results_ap_rate,
+                    results_path+"/"+"DGC_tests_results_ap_rate.dat",
+                    "# Gid, FR mean, ISI mean, ISI variance, ISI stdev, ISI adapt 1, ISI adapt 2, ISI adapt 3, ISI adapt 4")
 
 
 if __name__ == '__main__':
